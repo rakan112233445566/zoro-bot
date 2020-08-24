0972232e841b6481189f9b65b1f201f168906fa5
rakan112233445566 <70155245+rakan112233445566@users.noreply.github.com>
2020-08-24 19:10:16 +0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  TOKEN: "NzE5NTMxODIwMzcwMTAwMzI0.XxTjew.6B0nqgznOihMkODtSE6CxbDcjtI", //تعديل مهم توكن بوتك
-  YT_API_KEY: "api ",
-  prefix: "-",
-  devs: ["حط الايدي"] ///تعديل مهم غير هذا الرقم بـ ايدي حسابك
-};
